86347c2540410324aff542444130a87854192412
zeke <zeke@sikelianos.com>
2014-04-18 14:25:32 -0700

    rename web.js to index.js, a la npm defaults

--- index.js
+++ index.js
@@ -1,42 +1,12 @@
 var express = require('express');
 var app = express();
-var Twit = require('twit');
-var twitInfo = require('./config.js'); //add twitter app keys/tokens in config.js
+app.use(express.logger());
 
-// this is for heroku deployment, so we can see if it's running
-app.get('/', function(req, res){ res.send('Vinyl Digest bot is happily running.'); });
-app.listen(process.env.PORT || 5000);
-
-// reject tweets about that damn TV show...
-var regexReject = new RegExp('^.*(épisode|episode|hbo|@HBO|@vinylHBO|s01|@SkyAtlanticHD|@SkyAtlantic|TV|carpet|flooring|S1|#vinylHBO|RT|adult|xxx|porn|sexy|sex|cherry|magicstick|girlswithtattoos|pout|meulindo|sohot|sweet16|instabeauty|delish|eyelashes|tattedup).*$', 'i');
-
-var twitter = new Twit(twitInfo);
-
-var stream = twitter.stream('statuses/filter', {track: '#vinyl'}); // set the hashtag to retweet
-
-stream.on('connect', function(request) {
-	console.log('Connected to Twitter API');
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-stream.on('disconnect', function(request) {
-	console.log('Disconnected from Twitter API');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-
-stream.on('tweet', function(tweet){
-	var tweetID = tweet.id_str;
-
-	if (regexReject.test(tweet.text)) {
-		console.log('TWEET REJECTED!!!');
-		return;
-	} else {
-		console.log(tweetID);
-		twitter.post('statuses/retweet/:id', { id: tweetID }, function (err, data, response) {
-			tweetID = '';
-			console.log('TWEET POSTED!!!');
-		});
-	}
-})
-stream.on('error', function (tweet) {
-	console.log(tweet);
-});
-
