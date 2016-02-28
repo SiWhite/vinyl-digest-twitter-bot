77d1d67243978301442ec91c9494d4f62a0d2384
Simon White <simon.white@proximity.co.nz>
2016-02-29 10:26:13 +1300

    add twit back in

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Twit = require('twit');
 var twitInfo = require('./config.js'); //add twitter app keys/tokens in config.js
 
 app.set('port', (process.env.PORT || 5000));
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -14,20 +14,20 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-// var twitter = new Twit(twitInfo);
+var twitter = new Twit(twitInfo);
 
-// var stream = twitter.stream('statuses/filter', {track: '#vinyl'});
+var stream = twitter.stream('statuses/filter', {track: '#vinyl'});
 
-// stream.on('connect', function(request) {
-// 	console.log('Connected to Twitter API');
-// });
+stream.on('connect', function(request) {
+	console.log('Connected to Twitter API');
+});
 
-// stream.on('tweet', function(tweet){
-// 	var tweetID = tweet.id_str;
-// 	console.log(tweetID);
-// 	twitter.post('statuses/retweet/:id', { id: tweetID }, function (err, data, response) {
-// 		tweetID = '';.
-// 	})
-// })
+stream.on('tweet', function(tweet){
+	var tweetID = tweet.id_str;
+	console.log(tweetID);
+	twitter.post('statuses/retweet/:id', { id: tweetID }, function (err, data, response) {
+		tweetID = '';.
+	})
+})
 
 
