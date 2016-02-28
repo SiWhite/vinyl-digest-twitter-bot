471417174b6ba5831e6c48ed2dd1a55d9051f4b5
Simon White <simon.white@proximity.co.nz>
2016-02-29 10:07:19 +1300

    trying to add twit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: "LsPjPGkLLdkvaWn7Oj5qFgfSO",
+    consumer_secret: "HAPaI3Ww0tEtm0dzPyrkGnq3IPZ83NyUd4EJ0WLmHsrVE1s3vi",
+    access_token: "703482031011983360-gPT42o8UHsAxrAEXMV2ZDyZFL9S2sz",
+    access_token_secret: "JP5AF7W4lDVhH3lbFOFb0ZKqfMXbRwNQXUQp1Zn0peqGe"
+}
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
+var Twit = require('twit');
+var twitInfo = require('./config.js'); //add twitter app keys/tokens in config.js
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){ res.send('The robot is happily running.'); });
+//app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+/* app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
@@ -15,6 +17,6 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
+}); */
 
 
