404801741d00c2da7a46894f37a9a3c9cb721c0e
Simon White <simon.white@proximity.co.nz>
2016-03-01 09:36:22 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var Twit = require('twit');
+var twitInfo = require('./config.js'); //add twitter app keys/tokens in config.js
+app.get('/', function(req, res){ res.send('Vinyl Digest bot is happily running.'); });
+app.listen(process.env.PORT || 5000);
+
+var twitter = new Twit(twitInfo);
+
+var stream = twitter.stream('statuses/filter', {track: '#vinyl'});
+
+stream.on('connect', function(request) {
+	console.log('Connected to Twitter API');
+});
+
+stream.on('disconnect', function(request) {
+	console.log('Disconnected from Twitter API');
+});
+
+stream.on('tweet', function(tweet){
+	var tweetID = tweet.id_str;
+	console.log(tweetID);
+	twitter.post('statuses/retweet/:id', { id: tweetID }, function (err, data, response) {
+		tweetID = '';
+	})
+})
+stream.on('error', function (tweet) {
+	console.log(tweet);
+});
+
+
