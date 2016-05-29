6cf166c2f1b25f6ffc34326660066417a3ebbcc7
Si White <info@silentdesigns.co.nz>
2016-05-29 18:31:32 +1200

    add more filter terms

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res){ res.send('Vinyl Digest bot is happily running.'
 app.listen(process.env.PORT || 5000);
 
 // reject tweets about that damn TV show...
-var regexReject = new RegExp('^.*(show|Olivia|Wilde|Sky|ebay|episode|hbo|@HBO|@vinylHBO|s01|@SkyAtlanticHD|@SkyAtlantic|TV|carpet|flooring|S1|#vinylHBO|RT|Decal|banner|print|Scorsese|Laminate|binding|signs|nude|sex|meulino|cherry|xxx|adult|sexy|sohot|sweet16).*$', 'i');
+var regexReject = new RegExp('^.*(show|Olivia|Wilde|Sky|ebay|episode|hbo|@HBO|@vinylHBO|s01|@SkyAtlanticHD|@SkyAtlantic|TV|carpet|flooring|S1|#vinylHBO|RT|Decal|banner|print|Scorsese|Laminate|binding|signs|nude|sex|meulino|cherry|xxx|adult|sexy|sohot|sweet16|boombap|musicsupervisor).*$', 'i');
 
 var twitter = new Twit(twitInfo);
 
