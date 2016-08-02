fd87e03a1960b84c9eb8624ed285c741b6ff93bc
Simon White <simon.white@proximity.co.nz>
2016-08-02 15:27:52 +1200

    testing

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ Date.prototype.yyyymmdd = function() {
 var date = new Date();
 date = date.yyyymmdd();
 var prevTweetID = '';
+var tweet = null;
 
 setInterval(function() {
 	console.log('setInterval ran');
@@ -33,14 +34,14 @@ setInterval(function() {
 
 function reTweet() {
 	console.log('prevTweetID = '+prevTweetID);
-
 	twitter.get('/search/tweets', { q: '#vinyl since:'+date, count: 1, language: 'en' }, function(err, data, response){
-		var tweet = data.statuses[0];
+		tweet = data.statuses[0];
 		if (tweet !== undefined) {
 			var tweetID = tweet.id_str
 			console.log('tweetID = '+tweetID);
 			if ( regexReject.test(tweet.text) || tweetID === undefined || tweetID == prevTweetID ) {
 				console.log('TWEET REJECTED!!!');
+				tweet = null;
 				reTweet();
 			} else {
 				twitter.post('statuses/retweet/:id', { id: tweetID }, function (err, data, response) {
